dce5d974346d0a5911981259a191b07f6c094d27
rebeccawinterfield <34168846+rebeccawinterfield@users.noreply.github.com>
2017-12-19 13:16:44 -0500

    refactor jquery

--- script.js
+++ script.js
@@ -1,80 +1,87 @@
-let currentPageStudents = [];
+//define variables
 let currentPage = 1;
 let pageLength = 10;
+let count = 0 
 let students = $('.student-list').children();
 let numberOfPages = Math.ceil(students.length / 10);
-
-function removeStudents() {
-    $('.student-item').hide();
-}
-
-function errorMessage() {
-    let errorMessage = "";
-    errorMessage = $('.page h2').append('<p>Sorry, no students found.</p');
-}
-
-/*
-Function to hide students and then show first 10 on document load.
-*/
-$( document ).ready(function() {
-    console.log(students)
-    console.log(numberOfPages)
-    removeStudents();
-    for (let i = 0; i < pageLength; i++) {
-        let start = pageLength * (currentPage - 1);
-        $('.student-item').slice(start, pageLength).show();
-    }
-});
-
-// Add html and buttons to page dynamically. adding active class to active button. 
-// Append search bar to header on doc load.
-
+//append student search bar and button
+$('.page-header').append('<div class="student-search"><input class="searchInput" type="text" placeholder = "Search for students..."</div>');
+$('.student-search').append('<button class="searchBtn">Search</button>');
+//append page buttons to pagination div at bottom of page
 function appendPageLinks() {
     $('.pagination').append('<ul></ul>');
     for (let i = 1; i < numberOfPages + 1; i++) {
         $( ".pagination > ul" ).append('<li><a href="#" class="btn">'+[i]+'</a><li>');
     };
-    $('.pagination ul li a').click(function(){
-        $(".pagination ul li a").removeClass();
-        $(this).addClass("active");
-    });
-    $('.page-header').append('<div class="student-search"><input class="searchInput" type="text" placeholder = "Search for students..."</div>');
-    $('.student-search').append('<button class="searchBtn">Search</button>');
 };
-//calling the function
+//call the function to add page buttons
 appendPageLinks();
 
-// button click event. first hide all student-items. then set target as 
-// a clicked buttons number. the math formula starts each page at the correct
-// student index. then loop through the ten students to show on each page and 
-// turn their display on.
+//create functions to be used repeatedly
+    //hide the students
+function removeStudents() {
+    $('.student-item').hide();
+}
 
-$('.btn').on('click', function(event) {
-    removeStudents();    
-    let target = parseInt($(event.target).text())
-    let start = pageLength * (target - 1);    
+    //reveal an error message beneath the h2 when no students match search
+function errorMessage() {
+    let errorMessage = "";
+    errorMessage = $('.page h2').append('<p>Sorry, no students found.</p');
+}
+
+    //add correct students to page and paginate
+function addStudents(){
+    let start = pageLength * (currentPage - 1);    
     for (let i = start; i < (start +10); i++) {
         console.log(i);
         $('.student-item').eq(i).show();
     }
+}
+
+//when document loads remove full list of students and call add students/paginate function
+$( document ).ready(function() {
+    removeStudents();
+    addStudents();
 });
-var count = 0 
+
+// button click event. first remove active class and add it to the clicked button.
+// then remove students from page, and add students to the page corresponding to 
+// what button was clicked by checking the text of the button and converting it to a 
+//number
+$('.btn').on('click', function(event) {
+    $(".pagination ul li a").removeClass();
+    $(this).addClass("active");
+    removeStudents();    
+    currentPage = parseInt($(event.target).text())
+    addStudents()
+});
+
+//Event listener for searching using the keyup event handler, listenes for each letter input
+//searches the student list for corresponding students and dynamically shows the found students
+//the search buttons are removed. If no students are found the 'no student found' message appears
+//if the textinput is empty the students are removed and the pagination buttons and correct
+//students are shown.
 $(".searchInput").on('keyup', function(event) {
     removeStudents(); 
+    $('.pagination').hide();
     let search;
     let result;
     search = $(this).val().toLowerCase();
-    console.log(search);
     result = $('.student-list').find("h3:contains("+ search +")").parent().parent().show();
-    console.log(result);
+    if(search === '' ){
+        removeStudents();
+        addStudents();
+        $('.pagination').show();
+    } else {
     if ( result.length === 0) {
-        if (count != 1 ){
+        if (count === 0 ){
         count = 1
         $('.student-list').append('<p class="studentOff">No Stuents Match Your Search</p>');
         }
      }
         else {
             $('.studentOff').hide();
+            count = 0
         }
-    
+    }
 }); 
